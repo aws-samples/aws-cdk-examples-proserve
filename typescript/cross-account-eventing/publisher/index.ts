c2e7b2d1ca51c99e694b1572d0c3274d7b5ff64e
Jared Dickson <code@jareddickson.com>
2022-01-12 09:20:46 -0500

    Configure parameters

--- typescript/cross-account-eventing/publisher/index.ts
+++ typescript/cross-account-eventing/publisher/index.ts
@@ -1,31 +1,35 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import {App, CfnOutput, Stack, StackProps} from "aws-cdk-lib";
+import {App, CfnOutput, CfnParameter, Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
 import {Topic} from "aws-cdk-lib/aws-sns";
 import {AccountPrincipal, Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
 
 class CrossAccountEventingPublisherStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        const topic = new Topic(this, 'PublisherTopic', {});
+    const param = new CfnParameter(this, 'SubscriberAccountId', {
+      type: "String",
+      description: "The Account Id of the subscriber"
+    });
 
-        const statement = new PolicyStatement({
-            actions: ['sns:Subscribe'],
-            effect: Effect.ALLOW,
-            principals: [new AccountPrincipal('067120822963')],
-            resources: [topic.topicArn]
-        });
+    const topic = new Topic(this, 'PublisherTopic', {});
 
-        topic.addToResourcePolicy(statement);
+    const statement = new PolicyStatement({
+      actions: ['sns:Subscribe'],
+      effect: Effect.ALLOW,
+      principals: [new AccountPrincipal(param.valueAsString)],
+      resources: [topic.topicArn]
+    });
 
-        new CfnOutput(this, 'PublisherTopicArn', {
-            value: topic.topicArn
-        });
-    }
+    topic.addToResourcePolicy(statement);
+
+    new CfnOutput(this, 'PublisherTopicArn', {
+      value: topic.topicArn
+    });
+  }
 }
-
 
 const app = new App();
 new CrossAccountEventingPublisherStack(app, 'CrossAccountEventingPublisherStack');
--- typescript/cross-account-eventing/subscriber/index.ts
+++ typescript/cross-account-eventing/subscriber/index.ts
@@ -7,46 +7,46 @@ import {Effect, PolicyStatement, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 import {Queue} from "aws-cdk-lib/aws-sqs";
 
 class CrossAccountEventingSubscriberStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        const param = new CfnParameter(this,'PublisherTopicArn', {
-            type: "String",
-            description: "The SNS Topic ARN of the publisher"
-        })
+    const param = new CfnParameter(this, 'PublisherTopicArn', {
+      type: "String",
+      description: "The SNS Topic ARN of the publisher"
+    });
 
-        const topic = Topic.fromTopicArn(this, 'SubscriberExternalTopic', param.valueAsString);
+    const topic = Topic.fromTopicArn(this, 'SubscriberExternalTopic', param.valueAsString);
 
-        const dlq = new Queue(this, 'SubscriberDeadLetterQueue', {
-            retentionPeriod: Duration.days(14)
-        })
+    const dlq = new Queue(this, 'SubscriberDeadLetterQueue', {
+      retentionPeriod: Duration.days(14)
+    });
 
-        const queue = new Queue(this, 'SubscriberQueue', {
-            deadLetterQueue: {
-                queue: dlq,
-                maxReceiveCount: 3
-            },
-            retentionPeriod: Duration.days(7)
-        });
+    const queue = new Queue(this, 'SubscriberQueue', {
+      deadLetterQueue: {
+        queue: dlq,
+        maxReceiveCount: 3
+      },
+      retentionPeriod: Duration.days(7)
+    });
 
-        const statement = new PolicyStatement({
-            actions: ['sqs:SendMessage'],
-            effect: Effect.ALLOW,
-            principals: [new ServicePrincipal('sns.amazonaws.com')],
-            resources: [queue.queueArn]
-        });
-        statement.addCondition('ArnEquals', {
-            'aws:SourceArn': topic.topicArn,
-        });
+    const statement = new PolicyStatement({
+      actions: ['sqs:SendMessage'],
+      effect: Effect.ALLOW,
+      principals: [new ServicePrincipal('sns.amazonaws.com')],
+      resources: [queue.queueArn]
+    });
+    statement.addCondition('ArnEquals', {
+      'aws:SourceArn': topic.topicArn,
+    });
 
-        queue.addToResourcePolicy(statement);
+    queue.addToResourcePolicy(statement);
 
-        new Subscription(this, 'SubscriberSubscription', {
-            endpoint: queue.queueArn,
-            protocol: SubscriptionProtocol.SQS,
-            topic: topic
-        });
-    }
+    new Subscription(this, 'SubscriberSubscription', {
+      endpoint: queue.queueArn,
+      protocol: SubscriptionProtocol.SQS,
+      topic: topic
+    });
+  }
 }
 
 const app = new App();
