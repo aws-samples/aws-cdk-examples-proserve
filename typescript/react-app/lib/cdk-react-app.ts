04b277e26bf4530a0d349175f8378ceba70fc329
Daniel Immke <1744366+danielimmke@users.noreply.github.com>
2022-02-14 17:27:14 -0500

    React app example in Typescript (#13)
    * React app example in Typescript
    
    * Update README, fix spacing issue in file
    
    * Add diagram
    
    * Add cleanup section
    
    * Formatting cleanup

---
+++ typescript/react-app/lib/cdk-react-app.ts
@@ -0,0 +1,191 @@
+import {
+  Stack,
+  StackProps,
+  RemovalPolicy,
+  CfnOutput,
+  DockerImage,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { CachePolicy, Distribution, OriginAccessIdentity, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Architecture } from 'aws-cdk-lib/aws-lambda';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import {
+  CorsHttpMethod,
+  HttpApi,
+  HttpMethod,
+} from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { exec, execSync, ExecSyncOptions } from 'child_process';
+import { join } from 'path';
+import { copySync } from 'fs-extra';
+import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+export class CdkReactApp extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB Table
+    const table = new Table(this, 'NotesTable', {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      sortKey: { name: 'sk', type: AttributeType.STRING },
+      tableName: 'NotesTable',
+    });
+
+    // Lambdas
+    const readFunction = new NodejsFunction(this, 'ReadNotesFn', {
+      architecture: Architecture.ARM_64,
+      entry: `${__dirname}/lambda/readFunction.ts`,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    const writeFunction = new NodejsFunction(this, 'WriteNoteFn', {
+      architecture: Architecture.ARM_64,
+      entry: `${__dirname}/lambda/writeFunction.ts`,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    table.grantReadData(readFunction);
+    table.grantWriteData(writeFunction);
+
+    // API Gateway
+    const api = new HttpApi(this, 'NotesApi', {
+      corsPreflight: {
+        allowHeaders: ['Content-Type'],
+        allowMethods: [CorsHttpMethod.GET, CorsHttpMethod.POST],
+        allowOrigins: ['*'],
+      },
+    });
+    
+    const readIntegration = new HttpLambdaIntegration(
+      'ReadIntegration',
+      readFunction
+    );
+    
+    const writeIntegration = new HttpLambdaIntegration(
+      'WriteIntegration',
+      writeFunction
+    );
+    
+    api.addRoutes({
+      integration: readIntegration,
+      methods: [HttpMethod.GET],
+      path: '/notes',
+    });
+    
+    api.addRoutes({
+      integration: writeIntegration,
+      methods: [HttpMethod.POST],
+      path: '/notes',
+    });
+
+    // S3 Static Website
+    const staticFiles = new Bucket(this, 'frontend-app', {
+      autoDeleteObjects: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    const originAccessIdentity = new OriginAccessIdentity(
+      this,
+      'OriginAccessIdentity'
+    );
+
+    staticFiles.grantRead(originAccessIdentity);
+
+    // Cloudfront
+    const distribution = new Distribution(this, 'frontend-cdn', {
+      defaultBehavior: {
+        origin: new S3Origin(staticFiles, { originAccessIdentity }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: CachePolicy.CACHING_DISABLED // While developing
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ]
+    })
+
+    new CfnOutput(this, 'HttpApiUrl', { value: api.apiEndpoint });
+
+    // Build React
+    const execOptions: ExecSyncOptions = {
+      stdio: ['ignore', process.stderr, 'inherit'],
+    };
+    
+    const bundle = Source.asset(join(__dirname, 'web'), {
+        bundling: {
+        command: [
+            'sh',
+            '-c',
+            'echo "Docker build not supported. Please install esbuild."',
+        ],
+        image: DockerImage.fromRegistry('alpine'),
+        local: {
+            tryBundle(outputDir: string) {
+            try {
+                execSync('echo "Test?"');
+                execSync('esbuild --version', execOptions);
+            } catch {
+                return false;
+            }
+            execSync('npx vite build', execOptions);
+            copySync(join(__dirname, '/web/dist'), outputDir, {
+                ...execOptions,
+                recursive: true,
+            });
+            return true;
+            },
+        },
+        },
+    });
+
+    // Write built React files to S3 bucket
+    new BucketDeployment(this, 'DeployFiles', {
+      destinationBucket: staticFiles,
+      distribution,
+      logRetention: RetentionDays.ONE_DAY,
+      prune: false,
+      sources: [bundle],
+    })
+
+    // Add config.json file to S3 bucket as well
+    new AwsCustomResource(this, 'ApiUrlResource', {
+      logRetention: RetentionDays.ONE_DAY,
+      onUpdate: {
+        action: 'putObject',
+        parameters: {
+          Body: Stack.of(this).toJsonString({
+            [this.stackName]: { HttpApiUrl: api.apiEndpoint },
+          }),
+          Bucket: staticFiles.bucketName,
+          CacheControl: 'max-age=0, no-cache, no-store, must-revalidate',
+          ContentType: 'application/json',
+          Key: 'config.json',
+        },
+        physicalResourceId: PhysicalResourceId.of('config'),
+        service: 'S3',
+      },
+      policy: AwsCustomResourcePolicy.fromStatements([
+        new PolicyStatement({
+          actions: ['s3:PutObject'],
+          resources: [staticFiles.arnForObjects('config.json')],
+        }),
+      ]),
+    });
+
+    new CfnOutput(this, 'DistributionDomain', {
+      value: distribution.distributionDomainName,
+    });
+  }
+}
